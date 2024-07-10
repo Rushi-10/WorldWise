62c41cf0cc2a1499ebfc3de64db25fe583c21e2c
rushi10 <rushimadhure10@gmail.com>
2024-07-10 23:28:09 +0530

    updated citiesContext and use useReducer hook and also implement delete option for city

--- src/component/CityItem.jsx
+++ src/component/CityItem.jsx
@@ -14,15 +14,21 @@ export default function CityItem({city}){
             return (<img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt='flag' />)
         }
           
-     const{currentCity}=useCities(); 
+     const{currentCity,deleteCity}=useCities(); 
     const{cityName,emoji,date,id,position}=city;
+    function handleDelete(e){
+        e.preventDefault();
+        deleteCity(id);
+    }
     return(
         <li>
         <Link className={`${styles.cityItem} ${id===currentCity.id? styles["cityItem--active"]:''}`} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
         <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
+        <button onClick={handleDelete} className="deleteBtn">&times;</button>
         </Link>
+       
         </li>
     );
 }
